522cc2ce50d3cc8ee30c09af72a277ed585dc557
Glenn Ricaud <gri@enonic.com>
2015-09-18 10:48:14 +0200

    Commit

--- src/main/java/com/enonic/xp/lib/http/HttpClientHandler.java
+++ src/main/java/com/enonic/xp/lib/http/HttpClientHandler.java
@@ -1,6 +1,13 @@
 package com.enonic.xp.lib.http;
 
+import java.io.IOException;
 import java.util.Map;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 public final class HttpClientHandler
 {
@@ -26,9 +33,47 @@ public final class HttpClientHandler
     }
 
     public String execute()
+        throws IOException
     {
         System.out.println( "EXECUTE" );
+
+        final RequestBody requestBody = generateRequestBody();
+        sendRequest( url, requestBody );
+
         return "Request: " + method;
+
+    }
+
+    private RequestBody generateRequestBody()
+    {
+        final StringBuilder content = new StringBuilder( "{" );
+        for ( Map.Entry<String, String> param : params.entrySet() )
+        {
+            content.append( param.getKey() ).append( ":" ).append( param.getValue() );
+        }
+        content.append( "}" );
+        return RequestBody.create( MediaType.parse( "application/x-www-form-urlencoded; charset=utf-8" ), content.toString() );
+    }
+
+    private String sendRequest( final String url, final RequestBody post )
+        throws IOException
+    {
+        final OkHttpClient okHttpClient = new OkHttpClient();
+
+        //Builds the request
+        Request.Builder requestBuilder = new Request.Builder().url( url );
+        if ( post != null )
+        {
+            requestBuilder.post( post );
+        }
+        Request request = requestBuilder.build();
+
+        //Executes the request
+        Response response = okHttpClient.newCall( request ).execute();
+        System.out.println( "Message: " + response.message() );
+        final String responseBody = response.body().string();
+        System.out.println( "ResponseBody: " + responseBody );
+        return responseBody;
     }
 
 }
