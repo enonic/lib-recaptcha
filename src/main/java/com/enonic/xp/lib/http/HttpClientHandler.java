63ce7b20dc29f516c4a0754d8e67967535960e3b
Alex Rodriguez <aro@enonic.com>
2015-09-18 10:09:42 +0200

    httpclient parameters

--- src/main/java/com/enonic/xp/lib/http/HttpClientHandler.java
+++ src/main/java/com/enonic/xp/lib/http/HttpClientHandler.java
@@ -1,12 +1,28 @@
 package com.enonic.xp.lib.http;
 
+import java.util.Map;
+
 public final class HttpClientHandler
 {
     private String method;
 
+    private String url;
+
+    private Map<String, String> params;
+
     public void setMethod( final String method )
     {
         this.method = method;
+    }
+
+    public void setUrl( final String url )
+    {
+        this.url = url;
+    }
+
+    public void setParams( final Map<String, String> params )
+    {
+        this.params = params;
     }
 
     public String execute()
--- src/main/resources/site/lib/httpclient.js
+++ src/main/resources/site/lib/httpclient.js
@@ -2,7 +2,8 @@ exports.post = function (params) {
     var bean = __.newBean('com.enonic.xp.lib.http.HttpClientHandler');
 
     bean.method = 'post';
-    //bean.params = params;
+    bean.url = params.url;
+    bean.params = params.params;
 
     return __.toNativeObject(bean.execute());
 };
