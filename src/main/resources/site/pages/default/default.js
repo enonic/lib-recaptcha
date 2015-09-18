2e69e38ec084343b9dd71a3607394c66cc44ca7f
striky <tlo@enonic.com>
2015-09-18 12:59:27 +0200

    Removed unused code from default.js

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -1,5 +1,3 @@
-var UTIL = require('/lib/enonic/util/util');
-var menu = require('/lib/enonic/menu/menu');
 var portal = require('/lib/xp/portal');
 var thymeleaf = require('/lib/xp/thymeleaf');
 
@@ -24,32 +22,7 @@ function handleGet(req) {
 
         var model = {};
         model.mainRegion = me.content.page.regions['main'];
-        model.sitePath = me.site['_path'];
-        model.currentPath = me.content._path;
-        model.pageTitle = getPageTitle();
-        model.metaDescription = getMetaDescription();
-        model.menuItems = menu.getMenuTree(3);
-        model.siteName = me.site.displayName;
-
         return model;
-    }
-
-    function getPageTitle() {
-        return me.content['displayName'] + ' - ' + me.site['displayName'];
-    }
-
-    function getMetaDescription() {
-        var appNamePropertyName = app.name.replace(/\./g, '-');
-        var metaDescription = null;
-
-        if (me.content.x[appNamePropertyName]) {
-            if (me.content.x[appNamePropertyName]['html-meta']) {
-                if (me.content.x[appNamePropertyName]['html-meta']['htmlMetaDescription']) {
-                    metaDescription = me.content.x[appNamePropertyName]['html-meta']['htmlMetaDescription'];
-                }
-            }
-        }
-        return metaDescription;
     }
 
     return renderView();
