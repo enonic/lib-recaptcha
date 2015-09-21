ef818bb1f805e37b74d3e2b0555b6d7ac963593b
striky <tlo@enonic.com>
2015-09-21 10:05:24 +0200

    Removed httpclient.js

--- src/main/resources/site/lib/httpclient.js
+++
@@ -1,8 +0,0 @@
-exports.post = function (params) {
-    var bean = __.newBean('com.enonic.xp.lib.http.HttpClientHandler');
-
-    bean.url = params.url;
-    bean.params = params.params;
-
-    return __.toNativeObject(bean.execute());
-};
